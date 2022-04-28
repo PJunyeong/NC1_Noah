34cd8f2c606769ad883719a415fc0e00e7b71e74
PJunyeong <j_aion@naver.com>
2022-04-28 19:13:39 +0900

    feat: List Section in the NoteView

--- NC1_Noah/NC1_Noah/Views/NoteView.swift
+++ NC1_Noah/NC1_Noah/Views/NoteView.swift
@@ -9,29 +9,33 @@ import SwiftUI
 
 struct NoteView: View {
     @State private var selectedIndex = 0
-    let buttonLabels = [["10회", "20회", "30회", "40회", "50회"], ["유형1", "유형2", "유형3", "유형4", "유형5", "유형6"]]
+    let buttonLabels = [["10회", "20회", "30회", "40회", "50회"], ["10회", "20회", "30회", "40회", "50회"]]
     var body: some View {
         NavigationView{
-            VStack(spacing:50){
-                Picker("오답 및 책갈피", selection: $selectedIndex, content: {
+            VStack{
+                Picker("오답노트", selection: $selectedIndex, content: {
                     Text("오답")
                         .tag(0)
                     Text("책갈피").tag(1)
                 })
                 .pickerStyle(SegmentedPickerStyle())
                 .clipShape(RoundedRectangle(cornerRadius: 10))
-                .padding(20)
-
-                VStack{
+                
+                List{
                     ForEach(buttonLabels[selectedIndex], id: \.self) { buttonLabel in
-                        NavigationLink {
-                            Text("문제 푸는 곳")
-                        } label: {
-                            TestButtonLabel(buttonName: buttonLabel)
+                        Section(header: Text(buttonLabel)
+                        ){
+                            ForEach(buttonLabels[0], id: \.self){
+                                buttonLabel in
+                                NavigationLink {
+                                    Text("문제 푸는 곳")
+                                } label: {
+                                    Text(buttonLabel)
+                                }
+                            }
                         }
                     }
                 }
-                Spacer()
             }
             .navigationTitle(Text("오답노트"))
             .navigationBarTitleDisplayMode(.large)
@@ -39,8 +43,6 @@ struct NoteView: View {
         }
     }
 }
-
-
 struct NoteView_Previews: PreviewProvider {
     static var previews: some View {
         NoteView()
