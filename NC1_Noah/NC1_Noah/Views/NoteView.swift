a18888d43366c18c5a206b1b531f02c527dc9eb3
PJunyeong <j_aion@naver.com>
2022-04-28 14:38:26 +0900

    style: TestView 내 버튼 간 간격 조절 (spacing)

--- NC1_Noah/NC1_Noah/Views/NoteView.swift
+++ NC1_Noah/NC1_Noah/Views/NoteView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct NoteView: View {
+    @State private var selectedIndex = 0
+    let buttonLabels = [["10회", "20회", "30회", "40회", "50회"], ["유형1", "유형2", "유형3", "유형4", "유형5", "유형6"]]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack(spacing:50){
+                Picker("오답 및 책갈피", selection: $selectedIndex, content: {
+                    Text("오답")
+                        .tag(0)
+                    Text("책갈피").tag(1)
+                })
+                .pickerStyle(SegmentedPickerStyle())
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(20)
+
+                VStack{
+                    ForEach(buttonLabels[selectedIndex], id: \.self) { buttonLabel in
+                        NavigationLink {
+                            Text("문제 푸는 곳")
+                        } label: {
+                            TestButtonLabel(buttonName: buttonLabel)
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .navigationTitle(Text("오답노트"))
+            .navigationBarTitleDisplayMode(.large)
+            .font(.largeTitle)
+        }
     }
 }
+
 
 struct NoteView_Previews: PreviewProvider {
     static var previews: some View {
--- NC1_Noah/NC1_Noah/Views/TestView.swift
+++ NC1_Noah/NC1_Noah/Views/TestView.swift
@@ -12,16 +12,16 @@ struct TestView: View {
     let buttonLabels = [["10회", "20회", "30회", "40회", "50회"], ["유형1", "유형2", "유형3", "유형4", "유형5", "유형6"]]
     var body: some View {
         NavigationView{
-            VStack{
-                VStack{
-                    Picker("Favorite Color", selection: $selectedIndex, content: {
-                        Text("기출 모의고사").tag(0)
-                        Text("유형별").tag(1)
-                    })
-                    .pickerStyle(SegmentedPickerStyle())
-                    .clipShape(Rectangle())
-                    .padding(20)
-                }
+            VStack(spacing:50){
+                Picker("학습 유형", selection: $selectedIndex, content: {
+                    Text("기출 모의고사")
+                        .tag(0)
+                    Text("유형별").tag(1)
+                })
+                .pickerStyle(SegmentedPickerStyle())
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(20)
+
                 VStack{
                     ForEach(buttonLabels[selectedIndex], id: \.self) { buttonLabel in
                         NavigationLink {
@@ -35,7 +35,7 @@ struct TestView: View {
             }
             .navigationTitle(Text("학습"))
             .navigationBarTitleDisplayMode(.large)
-            .font(.title2)
+            .font(.largeTitle)
         }
     }
 }
